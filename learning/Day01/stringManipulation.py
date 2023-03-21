7ce8dae422e315d8c0fb0083a2c41f1788164b76
VINANK <neupaneayush3@gmail.com>
2023-03-21 16:20:58 +0545

    string manipulation

---
+++ learning/Day01/stringManipulation.py
@@ -0,0 +1,8 @@
+# printing the output in diffrent line
+print("Hello World!\nHello World!\nHello world!")
+
+# string concatenation
+print("Hello"+" "+"Ayush")
+
+# spaces are very important in python 
+# if we put space infront of the code then it will give indentation error
