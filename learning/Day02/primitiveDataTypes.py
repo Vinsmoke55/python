efddb4eaa81f0eb0e9b1200f95f79e6505914a06
VINANK <neupaneayush3@gmail.com>
2023-03-22 18:04:07 +0545

    python primitive data types

---
+++ learning/Day02/primitiveDataTypes.py
@@ -0,0 +1,20 @@
+# Data Types
+
+#strings
+#strings are always enclosed within quote'' or ""
+print("Ayush")
+print("Ayush"[0]) #to get the first letter of the string
+print("Ayush"[4]) #to get the last letter
+print("123"+"456")	#anything enclosed witin the quote is a string
+
+
+#Integers
+123
+print(123+456)
+
+#float
+print(3.1415)
+
+#boolean
+True
+False
