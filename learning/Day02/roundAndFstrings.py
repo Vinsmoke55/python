2a9df33fe0c039739a70a78e5dbdf16fc599f9bf
VINANK <neupaneayush3@gmail.com>
2023-03-22 19:15:01 +0545

    number manipulation and f strings

---
+++ learning/Day02/roundAndFstrings.py
@@ -0,0 +1,15 @@
+# rounding a number can be done by using round functio
+print(8/3)
+print(round(8/3))
+print(round(8/3,3)) #rounding a number upto three decimal places
+
+#floor division
+print(8//3)
+
+#Fstring
+#Fstring make it easier to mix up data of diffrent data types
+name="ayush"
+age=20
+height=1.8
+home=True
+print(f"my name is {name} my age is {age} my height is {height} i go home is {home}")
