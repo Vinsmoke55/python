268e89ebeaa375cbe53a2a8cc95f54c7316ba024
VINANK <neupaneayush3@gmail.com>
2023-03-23 18:23:17 +0545

    control flow with if-else

---
+++ learning/Day03/contorlFlow.py
@@ -0,0 +1,8 @@
+# control flow
+# control flow is use to control the flow of program with the given conditon
+print("Welcome to the Roller Coster")
+height=int(input("Enter your height in cm? "))
+if height>120:
+	print("You can ride the roller coaster")
+else:
+	print("You have to grow more taller to ride the roller coaster")
