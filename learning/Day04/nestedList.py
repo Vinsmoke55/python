693eb51d2dfd3900abd345da47634e52bd6638cc
VINANK <neupaneayush3@gmail.com>
2023-03-24 20:13:19 +0545

    nested lists

---
+++ learning/Day04/nestedList.py
@@ -0,0 +1,8 @@
+#nested lists are the list inside the list
+name=['ayush','ram','sita','hari']
+last_name=['neupane','khatri','thapa','hamal']
+
+person_detail=[name,last_name]
+
+print(person_detail[0][1])
+print(person_detail)
