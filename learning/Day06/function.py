586f8f2d51aac725f8f8acdd95f0dca89c516def
VINANK <neupaneayush3@gmail.com>
2023-03-26 18:58:29 +0545

    defining and calling function

---
+++ learning/Day06/function.py
@@ -0,0 +1,9 @@
+#function is a small piece of reusabel code
+print('hello world') #print() is also a function which is a built in function
+
+#making our own function
+def my_function():	#defining a function
+	print('hello')
+	print('world')
+
+my_function() #calling a function
