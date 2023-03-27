e42c5c4e0c0cee35324d2a29e2932dbadc9b1194
VINANK <neupaneayush3@gmail.com>
2023-03-27 19:13:22 +0545

    updated

--- project/HangMan/main.py
+++ project/HangMan/main.py
@@ -1,17 +1,25 @@
-#Step 1 
+#Step 2
+
 import random
 word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
 
-#To randomly choose a word from the word_list and assigning it to a variable called chosen_word.
-chosen_word=random.choice(word_list)
-print(chosen_word)
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-#To ask the user to guess a letter and assign their answer to a variable called guess. Making guess lowercase.
-guess=input("Guess the letter:").lower()
 
-#To check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+display=[]
 for letter in chosen_word:
-	if guess==letter:
-		print('right')
-	else:
-		print('wrong')
+	display+='-'
+print(display)
+
+guess = input("Guess a letter: ").lower()
+
+
+for position in range(len(chosen_word)):
+	letter=chosen_word[position]
+    if letter == guess:
+		display[position]=letter
+print(display)
+
+
