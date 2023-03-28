4b12776351e7a768e9a78d60814cd322c2a2743b
VINANK <neupaneayush3@gmail.com>
2023-03-28 19:01:37 +0545

    replacing the blanks

--- project/HangMan/main.py
+++ project/HangMan/main.py
@@ -1,25 +1,24 @@
-#Step 2
-
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+
+print(f'the solution is {chosen_word}.')
 
 
 display=[]
-for letter in chosen_word:
+word_length=len(chosen_word)
+for _ in range(word_length):
 	display+='-'
 print(display)
 
 guess = input("Guess a letter: ").lower()
 
 
-for position in range(len(chosen_word)):
+for position in range(word_length):
 	letter=chosen_word[position]
-    if letter == guess:
-		display[position]=letter
-print(display)
+	if letter==guess:
+		display[position]=guess
 
+print(display)
 
