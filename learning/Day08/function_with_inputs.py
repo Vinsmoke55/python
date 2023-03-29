e74ad67d2cb7659f31416ab0829a0082fd951ddc
VINANK <neupaneayush3@gmail.com>
2023-03-29 18:28:57 +0545

    function with inputs

---
+++ learning/Day08/function_with_inputs.py
@@ -0,0 +1,13 @@
+# simple function
+def greet():
+	print('hello')
+	print('how are you')
+
+greet()
+
+# function with name
+def greet_with_name(name):	#name is function parameter
+	print(f"Hello {name}")
+	print(f"How are you {name}")
+
+greet_with_name("ayush")	#"ayush" is function argument
