fdae4bdf4e41bee6083bc8626732d27923912d83
VINANK <neupaneayush3@gmail.com>
2023-03-31 18:27:00 +0545

    nesting list and dictionary

---
+++ learning/Day09/nesting_lists_and_dictionary.py
@@ -0,0 +1,24 @@
+# nesting list inside the dictionary
+travel_log={
+	"country":["nepal","india","japan"],
+	"no_of_visits":2
+}
+
+# nesting dictionary inside the dictionary
+travel_log={
+	"country":{"name":"nepal","no_of_visits":12}
+}
+
+# nesting dictionary inside the list
+travel_log=[
+{
+"name":"nepal",
+"no_of_visits":12
+},
+{
+"name":"india",
+"no_of_visits":1
+}
+]
+
+print(travel_log)
