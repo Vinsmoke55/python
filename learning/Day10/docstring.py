1cdc25add02bb7dec1aca5ecbb48db161a5e77d0
VINANK <neupaneayush3@gmail.com>
2023-04-01 17:01:15 +0545

    docstring

---
+++ learning/Day10/docstring.py
@@ -0,0 +1,14 @@
+#docstring is a piece of message that show up when we call the function
+# it can also be used as the multiline comment
+def name:
+	""" to display my name """
+	print("my name is ayush neupane")
+
+name()
+
+"""
+here i am writing
+a multiline comment
+
+"""
+a=10
