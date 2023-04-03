9fc50dd0b969bb3fe4c776badfedd52b4652f79c
VINANK <neupaneayush3@gmail.com>
2023-04-03 18:24:21 +0545

    no block scope

---
+++ learning/Day12/block_scope.py
@@ -0,0 +1,9 @@
+#there is no block scope in python
+num=3
+if num<5:
+	name="ayush"
+
+print(name)
+
+#this means that the variable created inside the if block for while loops etc
+#do not have local scope
