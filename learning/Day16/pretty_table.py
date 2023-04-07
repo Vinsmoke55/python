6b0be6e06f453a291b40777183a5217a416742cc
VINANK <neupaneayush3@gmail.com>
2023-04-07 19:26:45 +0545

    modifying object attributes and calling methods

---
+++ learning/Day16/pretty_table.py
@@ -0,0 +1,8 @@
+# installing and using a external package
+from prettytable import PrettyTable
+
+table=PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charamander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align="l"
+print(table)
