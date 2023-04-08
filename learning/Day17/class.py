4d593f5d12bb0878b56c93d73261e4947a494558
VINANK <neupaneayush3@gmail.com>
2023-04-08 18:01:12 +0545

    creating own class in python

---
+++ learning/Day17/class.py
@@ -0,0 +1,18 @@
+# creating a class
+class User:
+	pass	#pass keyword is used to leave class or function empty wihtout getting indentation error
+
+user_1=User()	#creating a object user_1
+
+'''
+MyName= pascalcase
+myName= camelcase
+my_name= snakecase
+
+
+pascalcasing is used to name a classname 
+and snake casing is used to name everything in python
+
+'''
+
+
