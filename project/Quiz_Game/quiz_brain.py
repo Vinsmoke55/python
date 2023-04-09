3db7259773e389b60856ccd303044db6468cd349
VINANK <neupaneayush3@gmail.com>
2023-04-09 18:43:46 +0545

    showing new questions

--- project/Quiz_Game/quiz_brain.py
+++ project/Quiz_Game/quiz_brain.py
@@ -3,6 +3,11 @@ class QuizBrain:
 		self.question_number=0
 		self.question_list=q_list 
 
+	def still_has_questions(self):
+		length=len(self.question_list)
+		return self.question_number<length
+		
+
 	def next_question(self):
 		current_question=self.question_list[self.question_number]
 		self.question_number+=1
