63ee5ca3cc296bd05a7edb20667960d6337b6237
VINANK <neupaneayush3@gmail.com>
2023-04-10 18:18:11 +0545

    understanding the turltle graphics

---
+++ learning/Day18/turtle_graphics.py
@@ -0,0 +1,18 @@
+from turtle import Turtle,Screen
+
+tim=Turtle()
+
+tim.shape("turtle")#changing the shape to turlte
+tim.color("red")	#changing the color of turtle to red
+tim.forward(100)	#making the turtle move forward by 100 paces
+tim.right(90)	#making the turtle turn right by 90 degree
+
+
+
+
+
+
+
+screen=Screen()
+screen.exitonclick()	#making the scrren hold and exit only after the click
+
