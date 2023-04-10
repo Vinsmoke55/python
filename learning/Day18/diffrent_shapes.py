4ce1137ab81633e3b5ef23bd04effb77d9a05fd2
VINANK <neupaneayush3@gmail.com>
2023-04-10 19:14:38 +0545

    drawing diffrent shapes

---
+++ learning/Day18/diffrent_shapes.py
@@ -0,0 +1,21 @@
+from turtle import Turtle,Screen
+
+tim=Turtle()
+
+angle=0
+sides=3
+condition=True
+while condition:
+	angle=360/sides
+	for _ in range(sides):
+		tim.forward(50)
+		tim.right(angle)
+	sides+=1
+	if sides==11:
+		condition=False
+	
+	
+
+
+screen=Screen()
+screen.exitonclick()
