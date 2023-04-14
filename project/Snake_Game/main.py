d443272417ea918d8fe9e023cc3603fc61ce3438
VINANK <neupaneayush3@gmail.com>
2023-04-14 17:51:35 +0545

    creating snake class and moving to OOP

--- project/Snake_Game/main.py
+++ project/Snake_Game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen,Turtle
 import time
+from snake import Snake
 
 screen=Screen()
 screen.setup(width=600,height=600)
@@ -7,27 +8,17 @@ screen.bgcolor("black")
 screen.tracer(0)
 screen.title("MY SNAKE GAME")
 
-starting_position=[(0,0),(-20,0),(-40,0)]
-segments=[]
+snake=Snake()
 
-for position in starting_position:
-	new_segment=Turtle(shape="square")
-	new_segment.color("white")
-	new_segment.penup()
-	new_segment.goto(position)
-	segments.append(new_segment)
+
+
 
 game_is_on=True
 while game_is_on:
 	screen.update()
 	time.sleep(0.1)
-	for seg_num in range(len(segments)-1,0,-1):
-		new_x=segments[seg_num-1].xcor()
-		new_y=segments[seg_num-1].ycor()
-		segments[seg_num].goto(new_x,new_y)
+	snake.move()
 
-	segments[0].forward(20)
-	segments[0].right(90)
 
 
 screen.exitonclick()
