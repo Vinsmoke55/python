e0ce457be8c47b85ed435e5b99d0e6a6b1683af1
VINANK <neupaneayush3@gmail.com>
2023-04-15 17:24:36 +0545

    finished project

--- project/Snake_Game/main.py
+++ project/Snake_Game/main.py
@@ -30,11 +30,19 @@ while game_is_on:
 	if snake.head.distance(food)<15:
 		food.refresh()
 		scoreboard.increase_score()
+		snake.extend()
 
 	#detect collision with the wall
 	if snake.head.xcor()>280 or snake.head.ycor()>280 or snake.head.xcor()<-280 or snake.head.ycor()<-280:
 		game_is_on=False
 		scoreboard.game_over()
+
+
+	#detect collision with the tail
+	for segment in snake.segments[1:]:
+		if snake.head.distance(segment)<10:
+			game_is_on=False
+			scoreboard.game_over()
 
 
 
