50456ef029d191a994013721ee31492be0f936fc
VINANK <neupaneayush3@gmail.com>
2023-04-15 17:32:01 +0545

    slicing the list and the tuples

---
+++ learning/Day21/slicing.py
@@ -0,0 +1,16 @@
+alphabet=["a","e","i","o","u","c","d"]
+
+print(alphabet[2:5]) #give the value from index 2 to 4
+
+print(alphabet[2:])	#give the value form index 2 to forward
+
+print(alphabet[:5])	#gives the value upto index 4
+
+print(alphabet[2:5:2])	#give the value from index 2 to 4 jumping 2 steps
+
+print(alphabet[:-2])	#gives the index upto index -1
+
+print(alphabet[-3:])	#gives the index from index -3
+
+
+#same thing can be done in tuples
