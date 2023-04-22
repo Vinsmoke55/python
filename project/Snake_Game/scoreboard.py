0c58a18b640da16eaaa6e9b8e8e84725df21f8d9
VINANK <neupaneayush3@gmail.com>
2023-04-22 18:44:44 +0545

    finished project updated with file of highscore

--- project/Snake_Game/scoreboard.py
+++ project/Snake_Game/scoreboard.py
@@ -2,11 +2,14 @@ from turtle import Turtle,Screen
 ALIGNMENT="center"
 FONT=("ariel",20,"normal")
 
+with open("data.txt") as file:
+	content=int(file.read())
+
 class ScoreBoard(Turtle):
 	def __init__(self):
 		super().__init__()
 		self.score=0
-		self.high_score=0
+		self.high_score=content
 		self.goto(0,270)
 		self.color("white")
 		self.scoreboard_text()
@@ -28,6 +31,9 @@ class ScoreBoard(Turtle):
 	def reset(self):
 		if self.score>self.high_score:
 			self.high_score=self.score
+			with open("data.txt",mode="w") as file:
+				file.write(f"{self.high_score}")
+
 		self.score=0
 		self.scoreboard_text()
 
