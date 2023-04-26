800a392fff462c004f46b2f875c6beaf7f807bc0
VINANK <neupaneayush3@gmail.com>
2023-04-26 19:19:20 +0545

    us state game

--- project/Us_State_Game/main.py
+++ project/Us_State_Game/main.py
@@ -1,7 +1,9 @@
 from turtle import Turtle,Screen
+import pandas
 
 screen=Screen()
 turtle=Turtle()
+t=Turtle()
 
 #setting up image as the background for the screen
 screen.title("Us State Game")
@@ -9,6 +11,22 @@ image="blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state=screen.textinput(title="Guess the state",prompt="What's another state name ? ")
 
-print(answer_state)
+
+
+data=pandas.read_csv("50_states.csv")
+states_list=data["state"].to_list()
+guessed_state=[]
+
+while len(guessed_state)<50:
+	guess=screen.textinput(title=f"{len(guessed_state)}/50 correct",prompt="What's another state name ? ").title()
+	
+	if guess in states_list:
+		correct_state=data[data.state==guess]
+		t.penup()
+		t.hideturtle()
+		t.goto(int(correct_state.x),int(correct_state.y))
+		t.write(guess)
+		guessed_state.append(guess)
+	
+		
