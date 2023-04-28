245ae773033e58c9b03a8c1e5a96fdd9cda3cced
VINANK <neupaneayush3@gmail.com>
2023-04-28 18:14:13 +0545

    adding list comprehension to the us state project

--- project/Us_State_Game/main.py
+++ project/Us_State_Game/main.py
@@ -22,11 +22,11 @@ while len(guessed_state)<50:
 	guess=screen.textinput(title=f"{len(guessed_state)}/50 correct",prompt="What's another state name ? ").title()
 	if guess=="Exit":
 		missed_state_dict={
-		"name":[]
+		"name":[state for state in states_list if state not in guessed_state]
 		}
-		for state in states_list:
-			if state not in guessed_state:
-				missed_state_dict["name"].append(state)
+		# for state in states_list:
+		# 	if state not in guessed_state:
+		# 		missed_state_dict["name"].append(state)
 		missed_state=pandas.DataFrame(missed_state_dict)
 		missed_state.to_csv("misseddata.csv")
 		break
