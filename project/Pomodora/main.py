031b1c30df3a58000482d1a12ffacf57ff5b4f2d
VINANK <neupaneayush3@gmail.com>
2023-05-11 17:38:45 +0545

    setting diffrent timer session and values

--- project/Pomodora/main.py
+++ project/Pomodora/main.py
@@ -9,12 +9,26 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps=0
 
 # ---------------------------- TIMER RESET ------------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-	count_down(1500)
+	global reps
+	reps+=1
+	work_sec=WORK_MIN*60
+	short_break_sec=SHORT_BREAK_MIN*60
+	long_break_sec=LONG_BREAK_MIN*60
+	if reps%8==0:
+		title_label.config(text="Break",fg=RED)
+		count_down(long_break_sec)
+	elif reps%2==0:
+		title_label.config(text="Break",fg=PINK)
+		count_down(short_break_sec)
+	else:
+		title_label.config(text="Work",fg=GREEN)
+		count_down(work_sec)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
@@ -25,6 +39,8 @@ def count_down(count):
 	canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
 	if count>0:
 		window.after(1000,count_down,count-1)
+	else:
+		start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
