2d5f71b1024e26e23fa3a7528001f5d4de3e9a93
VINANK <neupaneayush3@gmail.com>
2023-05-12 18:59:11 +0545

    finished project

--- project/Password_manager_GUI/main.py
+++ project/Password_manager_GUI/main.py
@@ -1,6 +1,30 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+
+def insert_random_password():
+	letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+	nr_letters= random.randint(0,10)
+	nr_symbols = random.randint(0,4)
+	nr_numbers = random.randint(0,4)
+
+	password_letter=[random.choice(letters) for _ in range(nr_letters)]
+	password_symbol=[random.choice(symbols) for _ in range(nr_symbols)]
+	password_numbers=[random.choice(numbers) for _ in range(nr_numbers)]
+
+	combination_password=password_letter+password_symbol+password_numbers
+	random.shuffle(combination_password)
+
+	random_password="".join(combination_password)
+
+	password_entry.delete(0,END)
+	password_entry.insert(0,f"{random_password}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -10,7 +34,7 @@ def save():
 
 	if len(website)==0 or len(password)==0:
 		messagebox.showwarning(message="Please do not leave any field empty.")
-		
+		return
 
 	is_ok=messagebox.askokcancel(title=website,message=f"The entered informaion is:\nwebsite:{website}\nEmail:{email}\nPassword:{password}\nDo you want to save ?")
 
@@ -51,7 +75,7 @@ password_label.grid(column=0,row=3)
 password_entry=Entry(width=21)
 password_entry.grid(column=1,row=3)
 
-generate_password=Button(text="Generate Password")
+generate_password=Button(text="Generate Password",command=insert_random_password)
 generate_password.grid(column=2,row=3)
 
 add_button=Button(text="Add",width=36,command=save)
