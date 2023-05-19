240a83a982ac11cb9ba58daac816038782509d58
VINANK <neupaneayush3@gmail.com>
2023-05-19 13:32:05 +0545

    flipping cards

--- project/Flash_card/main.py
+++ project/Flash_card/main.py
@@ -5,14 +5,22 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data=pandas.read_csv("./data/french_words.csv")
 to_learn=data.to_dict(orient="records")
+current_card={}
 
 def next_card():
+	global current_card,flip_timer
+	window.after_cancel(flip_timer)
 	current_card=random.choice(to_learn)
-	canvas.itemconfig(card_title,text=f"French")
-	canvas.itemconfig(card_word,text=f'{current_card["French"]}')
+	canvas.itemconfig(card_image,image=card_front)
+	canvas.itemconfig(card_title,text=f"French",fill="black")
+	canvas.itemconfig(card_word,text=f'{current_card["French"]}',fill="black")
+	flip_timer=window.after(3000,func=flip_card)
 
-
-
+def flip_card():
+	global current_card
+	canvas.itemconfig(card_image,image=card_back)
+	canvas.itemconfig(card_title,text=f"English",fill="white")
+	canvas.itemconfig(card_word,text=f'{current_card["English"]}',fill="white")
 
 
 # creating the user interface
@@ -20,10 +28,12 @@ window=Tk()
 window.title("Flashy")
 window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 
+flip_timer=window.after(3000,func=flip_card)
+
 canvas=Canvas(width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
 card_front=PhotoImage(file="./images/card_front.png")
 card_back=PhotoImage(file="./images/card_back.png")
-canvas.create_image(400,263,image=card_front)
+card_image=canvas.create_image(400,263,image=card_front)
 card_title=canvas.create_text(400,150,text="",font=("Arial",40,"italic"))
 card_word=canvas.create_text(400,263,text="",font=("Arial",60,"bold"))
 canvas.grid(row=0,column=0,columnspan=2)
