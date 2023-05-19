0ee71b45ca2d7474df23c4b89246e6fefbfa89d8
VINANK <neupaneayush3@gmail.com>
2023-05-19 13:09:47 +0545

    creating new flash cards

--- project/Flash_card/main.py
+++ project/Flash_card/main.py
@@ -1,6 +1,21 @@
 from tkinter import *
+import pandas
+import random
 BACKGROUND_COLOR = "#B1DDC6"
 
+data=pandas.read_csv("./data/french_words.csv")
+to_learn=data.to_dict(orient="records")
+
+def next_card():
+	current_card=random.choice(to_learn)
+	canvas.itemconfig(card_title,text=f"French")
+	canvas.itemconfig(card_word,text=f'{current_card["French"]}')
+
+
+
+
+
+# creating the user interface
 window=Tk()
 window.title("Flashy")
 window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
@@ -9,16 +24,18 @@ canvas=Canvas(width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
 card_front=PhotoImage(file="./images/card_front.png")
 card_back=PhotoImage(file="./images/card_back.png")
 canvas.create_image(400,263,image=card_front)
-canvas.create_text(400,150,text="french",font=("Arial",40,"italic"))
-canvas.create_text(400,263,text="trouve",font=("Arial",60,"bold"))
+card_title=canvas.create_text(400,150,text="",font=("Arial",40,"italic"))
+card_word=canvas.create_text(400,263,text="",font=("Arial",60,"bold"))
 canvas.grid(row=0,column=0,columnspan=2)
 
 right=PhotoImage(file="./images/right.png")
 wrong=PhotoImage(file="./images/wrong.png")
-right_button=Button(image=right,highlightthickness=0)
+right_button=Button(image=right,highlightthickness=0,command=next_card)
 right_button.grid(row=1,column=0)
 
-wrong_button=Button(image=wrong,highlightthickness=0)
+wrong_button=Button(image=wrong,highlightthickness=0,command=next_card)
 wrong_button.grid(row=1,column=1)
+
+next_card()
 
 mainloop()
