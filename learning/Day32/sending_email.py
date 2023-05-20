1bbc0ef27dc8c7ad3447fe11ea6e9593a5f708c7
VINANK <neupaneayush3@gmail.com>
2023-05-20 10:54:13 +0545

    sending an email from smtplib

---
+++ learning/Day32/sending_email.py
@@ -0,0 +1,13 @@
+
+import smtplib
+
+EMAIL="neupaneayush3@gmail.com"
+APPPASSWORD="qqyitcsxljkhkrwj"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+	connection.starttls()
+	connection.login(user=EMAIL,password=APPPASSWORD)
+	connection.sendmail(from_addr=EMAIL,
+		to_addrs="neupane_ayush@yahoo.com",
+		msg="Subject:hello\n\nhello world"
+		)
