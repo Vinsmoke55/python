b9a488c7b6f8dce9dd2f576292866cc2604d2d98
VINANK <neupaneayush3@gmail.com>
2023-05-22 15:20:19 +0545

    finished project

--- project/Quizzler_app/ui.py
+++ project/Quizzler_app/ui.py
@@ -11,8 +11,8 @@ class QuizInterface:
 		self.window.title("Quizzler")
 		self.window.config(padx=20,pady=20,bg=THEME_COLOR)
 
-		self.score=Label(text="score:0",fg="white",bg=THEME_COLOR)
-		self.score.grid(row=0,column=1)
+		self.qsn=Label(text=f"Score: 0",fg="white",bg=THEME_COLOR)
+		self.qsn.grid(row=0,column=1)
 
 		self.canvas=Canvas(height=250,width=300,bg="white")
 		self.question_text=self.canvas.create_text(150,120,text="some question",width=280,font=("Arial",20,"italic"),fill=THEME_COLOR)
@@ -33,11 +33,30 @@ class QuizInterface:
 		self.window.mainloop()
 
 	def get_next_question(self):
-		q_text=self.quiz.next_question()
-		self.canvas.itemconfig(self.question_text,text=q_text)
+		self.canvas.config(bg="white")
+		if self.quiz.still_has_questions():
+			self.qsn.config(text=f"Score: {self.quiz.score}")
+			q_text=self.quiz.next_question()
+			self.canvas.itemconfig(self.question_text,text=q_text)
+		else:
+			self.canvas.itemconfig(self.question_text,text="You have reached the end.")
+			self.true_button.config(state="disable")
+			self.false_button.config(state="disable")
 
 	def true_pressed(self):
-		self.quiz.check_answer("True")
+		is_right=self.quiz.check_answer("True")
+		self.give_feedback(is_right)
+
 	def false_pressed(self):
-		self.quiz.check_answer("False")
+		is_right=self.quiz.check_answer("False")
+		self.give_feedback(is_right)
+
+	def give_feedback(self,is_right):
+		if is_right==True:
+			self.canvas.config(bg="green")
+		else:
+			self.canvas.config(bg="red")
+		self.window.after(1000,self.get_next_question)
+		
+
 
