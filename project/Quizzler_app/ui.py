89beb317bb20bae32fb148357b18cdd6e48e9475
VINANK <neupaneayush3@gmail.com>
2023-05-22 14:10:35 +0545

    showing the next question in gui

--- project/Quizzler_app/ui.py
+++ project/Quizzler_app/ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-	def __init__(self):
+	def __init__(self,quizbrain:QuizBrain):
+		self.quiz=quizbrain
 		self.window=Tk()
 		self.window.title("Quizzler")
 		self.window.config(padx=20,pady=20,bg=THEME_COLOR)
@@ -13,7 +15,7 @@ class QuizInterface:
 		self.score.grid(row=0,column=1)
 
 		self.canvas=Canvas(height=250,width=300,bg="white")
-		self.question_text=self.canvas.create_text(150,120,text="some question",font=("Arial",20,"italic"),fill=THEME_COLOR)
+		self.question_text=self.canvas.create_text(150,120,text="some question",width=280,font=("Arial",20,"italic"),fill=THEME_COLOR)
 		self.canvas.grid(row=1,column=0,columnspan=2,pady=50)
 
 		false_img=PhotoImage(file="./images/false.png")
@@ -26,4 +28,11 @@ class QuizInterface:
 		self.false_button=Button(image=false_img,highlightthickness=0)
 		self.false_button.grid(row=2,column=1)
 
+		self.get_next_question()
+
 		self.window.mainloop()
+
+	def get_next_question(self):
+		q_text=self.quiz.next_question()
+		self.canvas.itemconfig(self.question_text,text=q_text)
+
