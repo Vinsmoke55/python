354a5d2884d7aeaf1803320d833c4db3661c217d
VINANK <neupaneayush3@gmail.com>
2023-05-24 16:14:15 +0545

    checking if it will rain in next 12 hours

--- learning/Day35/weather_api.py
+++ learning/Day35/weather_api.py
@@ -5,10 +5,25 @@ API_KEY="28a10b750cb699fcccf93c656372db0c"
 PARAMETER={
 	"lat":27.717245,
 	"lon":85.323959,
-	"appid":API_KEY
+	"appid":API_KEY,
+	"exclude":"daily,minutely,current"
 }
 
 response=requests.get(url=API_ENPOINT,params=PARAMETER)
 
-data=response.json()
-print(data)
+weather_data=response.json()
+
+carry_umbrella=False
+
+weather=weather_data["hourly"][:12]
+for hour_data in weather:
+	data=hour_data["weather"][0]["id"]
+	if data<700:
+		carry_umbrella=True
+
+if carry_umbrella:
+	print("Carry your umbrella")
+
+
+
+
