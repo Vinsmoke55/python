da55e8dcb1c685faf1a001d38611acd17ccb7ca8
VINANK <neupaneayush3@gmail.com>
2023-05-24 17:51:45 +0545

    working with the environment variable

--- learning/Day35/weather_api.py
+++ learning/Day35/weather_api.py
@@ -1,8 +1,9 @@
+import os
 import requests
 from twilio.rest import Client
 
 API_ENPOINT="https://api.openweathermap.org/data/2.5/onecall?lat=27.717245"
-API_KEY="28a10b750cb699fcccf93c656372db0c"
+API_KEY=os.environ.get('API')	#working with the environment variable
 PARAMETER={
 	"lat":27.717245,
 	"lon":85.323959,
@@ -23,12 +24,13 @@ for hour_data in weather:
 		carry_umbrella=True
 
 if carry_umbrella:
+	print("please carry your umbrealla")
 	account_sid = "AC128774b39c6633ab4b8ce97dc966f809"
 	auth_token ="f1579e2770ecdc77f50d180105035b6a"
 	client = Client(account_sid, auth_token)
 	message = client.messages.create(
 	  body="Its going to rain please carry your umbrella",
 	  from_="+13156673970",
-	  to="+9779744388789"
+	  to=os.environ.get('MY_PHONE_NO')
 	)
-	print(message.status)
+	# print(message.status)
