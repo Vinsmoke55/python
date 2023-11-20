a400e340e2375af722d79e00b8b4680bb62de801
VINANK <neupaneayush3@gmail.com>
2023-11-20 16:48:42 +0545

    finding and selecting elements on website with selenium

--- learning/Day48/setup.py
+++ learning/Day48/setup.py
@@ -1,5 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver=webdriver.Chrome()
 
-driver.get("https://www.amazon.com")
+driver.get("https://www.python.org/")
+
+logo=driver.find_element(By.CLASS_NAME,"python-logo")
+
+documentation_link=driver.find_element(By.CSS_SELECTOR,".documentation-widget a")
+
+bug=driver.find_element(By.XPATH,'//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+
+print(logo.get_attribute("src"))
+
+print(logo.size)
+
+print(documentation_link.text)
+
+print(bug.text)
